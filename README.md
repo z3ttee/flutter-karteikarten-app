a32923c558e9de3a094b9e6a2083495814d1c73a
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-02-11 23:31:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter-karteikarten-app
+ 
---
+++ android/app/src/main/java/dev/zitzmann/flutter_karteikarten_app/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.zitzmann.flutter_karteikarten_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
