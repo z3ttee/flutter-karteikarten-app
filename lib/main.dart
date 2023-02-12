e081f17d17e508e34905768e05394f23de6e1d72
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-02-12 20:55:17 +0100

    added routes + fab + use device theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_karteikarten_app/behaviours/pointerScrollBehaviour.dart';
-import 'package:flutter_karteikarten_app/views/moduleList.dart';
+import 'package:flutter_karteikarten_app/screens/moduleListScreen.dart';
 
 void main() {
-  // Add google fonts licence to the app
-  LicenseRegistry.addLicense(() async* {
-    final licenseRobotoFlex = await rootBundle.loadString('google_fonts/ROBOTO_FLEX_OFL.txt');
-    final licenseRobotoSerif = await rootBundle.loadString('google_fonts/ROBOTO_SERIF_OFL.txt');
-
-    yield LicenseEntryWithLineBreaks(['google_fonts'], licenseRobotoFlex);
-    yield LicenseEntryWithLineBreaks(['google_fonts'], licenseRobotoSerif);
-  });
-
-  // Disable fetching of google fonts at runtime (only
-  // bundled fonts are used)
-  // GoogleFonts.config.allowRuntimeFetching = false;
-
   // Run the app
   runApp(const MyApp());
 }
@@ -30,53 +15,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Karteikarten App',
+      // Light theme settings
       theme: ThemeData(
+        // Set theme as light-theme
+        brightness: Brightness.light,
         // Material Design 3 verwenden
         useMaterial3: true,
         colorSchemeSeed: Colors.orange,
         // Use Material Design 3 typography settings
         typography: Typography.material2021(),
       ),
+      // Dark-Theme settings
+      darkTheme: ThemeData(
+        // Set theme as dark-theme
+        brightness: Brightness.dark,
+        // Material Design 3 verwenden
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepOrange,
+        // Use Material Design 3 typography settings
+        typography: Typography.material2021(),
+      ),
+      // Pick light or dark theme based on device's settings
+      themeMode: ThemeMode.system,
+      // Hide "debug" banner
+      debugShowCheckedModeBanner: false,
       // Allow scrolling via mouse-dragging on devices with cursors
       scrollBehavior: PointerScrollBehaviour(),
-      home: const MyHomePage(title: 'Modulübersicht'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Karteikarten", style: TextStyle(),),
-      ),
-      body: const ModuleListView(),
+      routes: {
+        '/': (context) => const ModuleListScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/moduleListScreen.dart
@@ -0,0 +1,91 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_karteikarten_app/widgets/moduleItemCard.dart';
+
+class ModuleListScreen extends StatelessWidget {
+  const ModuleListScreen({super.key});
+
+  Future<List<dynamic>> _fetchListItems() async {
+    return Future.delayed(const Duration(milliseconds: 500), () {
+      return [];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: const Icon(Icons.add)
+      ),
+      body: FutureBuilder(
+          future: _fetchListItems(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            } else {
+              if((snapshot.data?.length ?? 0) > 0) {
+                return CustomScrollView(
+                  slivers: <Widget>[
+                    SliverAppBar.medium(
+                      title: const Text("Modulübersicht"),
+                      actions: <Widget>[
+                        IconButton(onPressed: () {}, icon: const Icon(Icons.info_outline))
+                      ],
+                    ),
+                    SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+                        int maxIndex = (snapshot.data?.length ?? 1) - 1;
+                        return Padding(
+                          padding: EdgeInsets.only(left: 12, right: 12, top: (index == 0) ? 12 : 0, bottom: (index == maxIndex) ? 12 : 0),
+                          child: ModuleItemCard(
+                            name: "Modul #${index + 1}",
+                            filled: true,
+                          ),
+                        );
+                      },
+                      childCount: snapshot.data?.length ?? 0
+                    )),
+                  ],
+                );
+              } else {
+                return CustomScrollView(
+                  slivers: <Widget>[
+                    SliverAppBar.medium(
+                      title: const Text("Modulübersicht"),
+                      centerTitle: true,
+                      scrolledUnderElevation: 4,
+                      actions: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(left: 4, right: 12),
+                          child: IconButton(onPressed: () {}, icon: const Icon(Icons.info_outline_rounded)),
+                        )
+                      ],
+                    ),
+                    SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+                      int maxIndex = 9;
+                      return Padding(
+                        padding: EdgeInsets.only(
+                            left: 12,
+                            right: 12,
+                            top: (index != 0) ? 0 : 12,
+                            bottom: (index != maxIndex) ? 0 : 12
+                        ),
+                        child: ModuleItemCard(
+                          name: "Modul #${index + 1}",
+                          filled: true,
+                        ),
+                      );
+                    },
+                        childCount: 10
+                    )),
+                  ],
+                );
+              }
+            }
+          }
+      ),
+    );
+  }
+
+}
+
