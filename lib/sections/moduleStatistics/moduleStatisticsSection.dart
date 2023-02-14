17bbae7d7418716becd64f0a5e46731d9c502fac
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2023-02-14 23:40:18 +0100

    less unused space

--- lib/sections/moduleStatistics/moduleStatisticsSection.dart
+++ lib/sections/moduleStatistics/moduleStatisticsSection.dart
@@ -16,18 +16,18 @@ class ModuleStatisticsSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.all(14),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Statistiken", style: Theme.of(context).textTheme.titleLarge,),
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 12), child: Text("Statistiken", style: Theme.of(context).textTheme.titleLarge,),),
           const SizedBox(height: 6,),
           Row(
             children: [
               Expanded(child: StatCard(title: "Karten", value: "${module.cards.length}")),
               Expanded(child: StatCard(title: "Durchläufe", value: "${module.iterations}")),
-              Expanded(child: StatCard(title: "Abgeschlossen", value: "${Calc.calcModuleProgress(module)}", unit: "%",)),
+              Expanded(child: StatCard(title: "Fortschritt", value: "${Calc.calcModuleProgress(module)}", unit: "%",)),
             ],
           )
         ],
--- lib/widgets/cards/statisticsCard.dart
+++ lib/widgets/cards/statisticsCard.dart
@@ -22,14 +22,14 @@ class StatCard extends StatelessWidget {
     return SizedBox(
       width: width,
       child: Card(
-        elevation: 6,
+        elevation: 2,
         child: Padding(
-          padding: const EdgeInsets.all(Constants.cardInnerPadding),
+          padding: const EdgeInsets.all(Constants.cardInnerPadding - 3),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(title, overflow: TextOverflow.fade, maxLines: 1, softWrap: false, style: Theme.of(context).textTheme.labelMedium?.merge(TextStyle(color: Theme.of(context).colorScheme.onSurfaceVariant, letterSpacing: 1.5)),),
+              Text(title, overflow: TextOverflow.ellipsis, maxLines: 1, softWrap: false, style: Theme.of(context).textTheme.labelMedium?.merge(TextStyle(color: Theme.of(context).colorScheme.onSurfaceVariant, letterSpacing: 1.5)),),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
